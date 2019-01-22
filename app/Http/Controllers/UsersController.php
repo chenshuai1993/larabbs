bbc6562acba05d0ed07fed70d3d10e3be730692a
vagrant <vagrant@homestead>
2019-01-22 13:45:14 +0000

    用户个人页面原型

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UsersController extends Controller
+{
+    //
+    public function show(User $user)
+    {
+    	# code...
+    	return view('users.show', compact('user'));
+    	
+    }
+}
