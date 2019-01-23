8aed5a3fa7b68e4920a957d6f9201a7c5140dd7c
vagrant <vagrant@homestead>
2019-01-23 11:17:17 +0000

    授权访问

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,12 @@ use App\Handlers\ImageUploadHandler; #工具类
 
 class UsersController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['show']]);
+    }
+
     //
     public function show(User $user)
     {
@@ -20,13 +26,14 @@ class UsersController extends Controller
     public function edit(User $user)
     {
     	# code...
+        $this->authorize('update', $user);
     	return view('users.edit', compact('user'));
     	
     }
 
     public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
-    	
+    	$this->authorize('update', $user);
         $data = $request->all();
 
         if ($request->avatar) {
